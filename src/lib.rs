45613da76d2818f97bf05a2553dfe356efe4d228
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-25 20:31:44 +0500

    upd

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+pub use std::ops::Range;
+
+pub fn search(range: Range<f64>, eps: f64, f: impl Fn(f64) -> f64) -> f64 {
+    let invphi = 2.0 / (1.0 + 5.0f64.sqrt());
+    let invphi2 = invphi.powi(2);
+
+    let Range{ mut start, mut end } = range;
+    let mut x1 = start + invphi2 * (end - start);
+    let mut x2 = start + invphi * (end - start);
+    let mut f1 = f(x1);
+    let mut f2 = f(x2);
+    while eps < end - start {
+        if f1 < f2 {
+            end = x2;
+            x2 = x1;
+            f2 = f1;
+            x1 = start + invphi2 * (end - start);
+            f1 = f(x1);
+        } else {
+            start = x1;
+            x1 = x2;
+            f1 = f2;
+            x2 = start + invphi * (end - start);
+            f2 = f(x2);
+        }
+    }
+
+    0.5 * (start + end)
+}
